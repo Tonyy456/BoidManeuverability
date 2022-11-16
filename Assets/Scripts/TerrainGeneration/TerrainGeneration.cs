ad280c75be6d83ce6f60c940beed637bf1432fc1
Tonyy456 <ajdalesandro0115@gmail.com>
2022-11-16 15:55:54 -0500

    working on terrain generation

---
+++ Assets/Scripts/TerrainGeneration/TerrainGeneration.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class TerrainGeneration : MonoBehaviour
+{
+    [SerializeField] private bool regenerate = false;
+
+    [Header("GEOMETRY")]
+    [SerializeField] private int verticesWide = 10;
+    [SerializeField] private int verticesLong = 10;
+    [SerializeField] private float vertexSeperation = 10f;
+    [SerializeField] private float heightScalar = 100f;
+
+    [Header("NOISE")]
+    [SerializeField] private float frequency = 0.01f;
+    [SerializeField] private float seed = 0f;
+    public Texture2D texture;
+
+    public void Start()
+    {
+        GenerateMesh();
+    }
+
+    public void OnValidate()
+    {
+        if (!regenerate) return;
+        regenerate = false;
+        GenerateMesh();
+    }
+
+    private void GenerateMesh()
+    {
+        MeshGenerator generator = new MeshGenerator(verticesWide, verticesLong, vertexSeperation, new Vector3(0, 0, 0));
+        TerrainHeightGenerator terrain = new TerrainHeightGenerator(generator.GetMesh(), heightScalar, frequency);
+        terrain.GenerateHeight(GenerationType.XSquared);
+        GetComponent<MeshFilter>().mesh = terrain.GetMesh();
+    }
+}
---
+++ Assets/Scripts/TerrainGeneration/TerrainHeightGenerator.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GenerationType
+{
+    XSquared,
+    InverseX
+}
+
+public enum TerrainColoringType
+{
+    BrownToGreen
+}
+public class TerrainHeightGenerator
+{
+    private Mesh mesh;
+    private float seed;
+    private float frequency;
+    private float heightScalar;
+    public TerrainHeightGenerator(Mesh mesh, float heightScalar, float frequency = 0.1f, float seed = -1)
+    {
+        this.mesh = mesh;
+        this.frequency = frequency;
+        this.heightScalar = heightScalar;
+        if (seed < 0)
+            this.seed = Random.Range(0, (float)10e10);
+        else
+            this.seed = seed;
+    }
+
+    public Mesh GetMesh()
+    {
+        return mesh;
+    }
+
+    public void GenerateHeight(GenerationType type)
+    {
+        switch (type)
+        {
+            case GenerationType.XSquared:
+                GenerateUsingXSquared();
+                break;
+            case GenerationType.InverseX:
+                GenerateUsingInverseX();
+                break;
+            default:
+                GenerateUsingXSquared();
+                break;
+        }
+        UpdateMesh();
+    }
+
+    public void ColorTerrain(TerrainColoringType type)
+    {
+        switch (type)
+        {
+            case TerrainColoringType.BrownToGreen:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.RecalculateNormals();
+    }
+
+    private void GenerateUsingXSquared()
+    {
+        Vector3[] vertices = mesh.vertices;
+        for(int i = 0; i < mesh.vertexCount; i++)
+        {
+            Vector3 position = vertices[i];
+            float perlin = GetPerlinValueAt(position.x, position.y);
+            float newHeight = perlin * perlin * heightScalar;
+            vertices[i].y = newHeight;
+        }
+        mesh.vertices = vertices;
+    }
+
+    private void GenerateUsingInverseX()
+    {
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            Vector3 position = mesh.vertices[i];
+            float perlin = GetPerlinValueAt(position.x, position.y);
+            float newHeight = heightScalar / perlin;
+            mesh.vertices[i].y = newHeight;
+        }
+    }
+
+    private void GreenBrownColoring()
+    {
+        float maxHeight = float.MaxValue;
+        float minValue = float.MinValue;
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            if (mesh.vertices[i].y < maxHeight)
+            {
+                maxHeight = mesh.vertices[i].y;
+            }
+            if (mesh.vertices[i].y > minValue)
+            {
+                minValue = mesh.vertices[i].y;
+            }
+        }
+
+        for(int i = 0; i < mesh.vertexCount; i++)
+        {
+            //color
+        }
+    }
+
+    private float GetPerlinValueAt(float x, float y)
+    {
+        return Mathf.PerlinNoise((x * frequency), (y * frequency));
+    }
+}
