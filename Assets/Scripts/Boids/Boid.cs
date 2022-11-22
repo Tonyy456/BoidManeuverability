bbb1885ec0854885865baaf91aba283750033339
Tonyy456 <ajdalesandro0115@gmail.com>
2022-11-21 20:15:58 -0500

    boid defintiion

---
+++ Assets/Scripts/Boids/Boid.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    /*
+     * list of transforms for boids near me
+     *    - List<Transform>
+     * 
+     * list of raycast directions that missed (open paths)
+     *    - List<Vector3>
+     * 
+     * list of raycasts that hit a obstacle (OPTIONAL)
+     *    - List<RaycastHit>
+     *    
+     *  Debug.DrawRay()
+     */
+}
