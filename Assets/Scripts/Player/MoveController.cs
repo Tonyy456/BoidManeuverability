ad520c32947b4fbf788c94f86b505c31e0d49199
Taylor Liu <77754369+taystay@users.noreply.github.com>
2022-12-05 13:46:22 -0500

    Tweaks
    Fixed jumping in mid air, added a side view camera, and made look sensitivity better (still not 100%).

--- Assets/Scripts/Player/MoveController.cs
+++ Assets/Scripts/Player/MoveController.cs
@@ -42,7 +42,8 @@ public class MoveController : MonoBehaviour {
     private void Jump(InputAction.CallbackContext e) {
         targetMoveTowards.position = jumpHeight * targetToMove.up;
 
-        targetToMove.GetComponent<Rigidbody>().AddForce(targetMoveTowards.position);
+        if (Physics.Raycast(targetToMove.position, targetToMove.up * -1, 1f))
+            targetToMove.GetComponent<Rigidbody>().AddForce(targetMoveTowards.position);
     }
 
     //Movees the player at every fixed update towards the direction that the keyboard indicates
